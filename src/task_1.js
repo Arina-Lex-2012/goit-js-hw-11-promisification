bfba19d5c01b3d1d69af642327ca79232ab844c2
Maryna Berezhanska <arina.lex2012@gmail.com>
2021-05-15 23:24:06 +0300

    // npm run dev

--- src/task_1.js
+++ src/task_1.js
@@ -16,3 +16,5 @@ const delay = ms => {
   delay(2000).then(logger); // Resolved after 2000ms
   delay(1000).then(logger); // Resolved after 1000ms
   delay(1500).then(logger); // Resolved after 1500ms
+
+  // npm run dev
